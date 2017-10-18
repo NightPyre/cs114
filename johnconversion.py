b328399157fdfab517c4572c222aad86ce352cc1
s203 <s203@AI241LAB0203M15.local>
2017-10-18 11:56:15 -0700

    updates

---
+++ johnconversion.py
@@ -0,0 +1,7 @@
+print("I can convert gallons to liters")
+print("enter gallons")
+print(" ")
+gallons = int(input())
+liters=(gallons * 3.78541)
+print("liters", liters )'
+print("conversion complete")
