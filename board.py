ebbd11b7537fbf140ed13d3a643a33176a9a64b9
s203 <s203@AI241LAB0203M15.local>
2017-11-01 11:35:48 -0700

    iupdate

---
+++ board.py
@@ -0,0 +1,95 @@
+import random
+def getAnswer(answerNumber):
+
+        if answerNumber == 1:
+            return 'slime'
+
+        elif answerNumber == 2:
+            return 'corpse'
+
+        elif answerNumber == 3:
+            return 'kobold'
+
+r = random.randint(1, 3)
+enemy = getAnswer(r)
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(enemy)
+print(" ")
+
+print("BATTLE START")
+input()
+
+def getAnswer(answerNumber):
+
+        if answerNumber == 1:
+            return '2'
+
+        elif answerNumber == 2:
+            return '3'
+
+        elif answerNumber == 3:
+            return '4'
+
+        elif answerNumber == 4:
+            return '5'
+
+        elif answerNumber == 5:
+            return '6'
+
+        elif answerNumber == 6:
+            return '7'
+
+        elif answerNumber == 7:
+            return '8'
+
+        elif answerNumber == 8:
+            return '9'
+
+        elif answerNumber == 9:
+            return '10'
+
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print("cards")
+print(fortune)
+
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
---
+++ written_numbers.py
@@ -0,0 +1,117 @@
+print("please input a number between 1 and 99")
+num = int(input())
+tens = num // 10
+ones = num % 10
+
+#if num == 10:
+#    return 'ten'
+
+#elif num == 11:
+#    return 'eleven'
+
+#elif num == 12:
+#    return 'twelve'
+
+#elif num == 13:
+#    return 'thirteen'
+
+#elif num == 14:
+    #return 'fourteen'
+
+#elif num == 15:
+    #return 'fifteen'
+
+#elif num == 16:
+    #return 'sixteen'
+
+#elif num == 17:
+    #return 'seventeen'
+
+#elif num == 18:
+    #return 'eighteen'
+
+#elif num == 19:
+    #return 'nineteen'
+
+def getAnswer(answerNumber):
+    if tens == 1:
+        return 'ten'
+
+    elif tens == 2:
+        return 'twenty'
+
+    elif tens  == 3:
+        return 'thirty'
+
+    elif tens  == 4:
+        return 'forty'
+
+    elif tens == 5:
+        return 'fifty'
+
+    elif tens  == 6:
+        return 'sixty'
+
+    elif tens  == 7:
+        return 'seventy'
+
+    elif tens  == 8:
+        return 'eighty'
+
+    elif tens  == 9:
+        return 'ninty'
+ten = getAnswer(tens)
+
+def getAnswer(answerNumbers):
+    if ones == 1:
+        return 'one'
+
+    elif ones == 2:
+        return 'two'
+
+    elif ones == 3:
+        return 'three'
+
+    elif ones  == 4:
+        return 'four'
+
+    elif ones == 5:
+        return 'five'
+
+    elif ones  == 6:
+        return 'six'
+
+    elif ones == 7:
+        return 'seven'
+
+    elif ones  == 8:
+        return 'eight'
+
+    elif ones  == 9:
+        return 'nine'
+one = getAnswer(ones)
+
+if tens == 0:
+    output = ones
+elif tens == 1:
+    if ones == 1:
+        output = 'eleven'
+    elif ones == 2:
+        output = 'twelve'
+    elif ones == 3:
+        output = 'thirteen'
+    elif ones == 5:
+        output = 'fifteen'
+    elif ones == 0:
+        output = 'ten'
+    else:
+        output = one + 'teen'
+else:
+    output = tens+ '-' + ones
+
+print(output)
+#if num < 10:
+#    print(one)
+
+#else:
+#    print (ten + one)
